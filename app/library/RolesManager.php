02a4dd8684a1348bc7c066011c448e1436d9520d
Mohamad Mantach <itmsolutionslb@gmail.com>
2025-07-12 23:59:02 +0300

    fix callcenter module , products module ,crm module

--- app/library/RolesManager.php
+++ app/library/RolesManager.php
@@ -7,9 +7,9 @@
  Date Created : Apr 27, 2019
  Developed By  : Mohamad Mantach   PHP Department itm Solutions
  All Rights Reserved ,   itm Solutions COPYRIGHT 2019
- 
+
  Page Description :
- 
+
  ***********************************************************/
 
 namespace App\library;
@@ -24,16 +24,16 @@ use Crypt;
 use Cookie;
 use Auth;
 use DB;
-use File;  
+use File;
 use Illuminate\Support\Facades\Hash;
-use App\Models\Roles\PrivilegedActions;
-use App\Models\Roles\RolePrivileges;
-use App\Models\Roles\Roles;
+use App\models\Roles\PrivilegedActions;
+use App\models\Roles\RolePrivileges;
+use App\models\Roles\Roles;
 
 
 class RolesManager
 {
-    
+
     /**
      * get all privilege Actions Saved in the database
      *
@@ -45,9 +45,9 @@ class RolesManager
     public static function getAllPrivilegeAction()
     {
         $pa_array = PrivilegedActions::all();
- 
+
         $db_result_array = array();
-        
+
         $index = 0;
         foreach ($pa_array as $key => $pa_info) {
             $db_result_array[ $pa_info->pa_group ][$index]['id'] = $pa_info->pa_id;
@@ -56,32 +56,32 @@ class RolesManager
             $db_result_array[ $pa_info->pa_group ][$index]['description'] = $pa_info->pa_description;
             $index++;
         }
-        
-        
+
+
         return $db_result_array;
     }
-    
-    
+
+
     public static function getAllRolePrivilege( $role_id )
     {
         $rp_array = RolePrivileges::whereFkRoleRId($role_id)->get();
-        
+
         $db_result_array = array();
-         
-        foreach ( $rp_array as $key =>  $rp_info ) 
+
+        foreach ( $rp_array as $key =>  $rp_info )
         {
             $db_result_array[ $rp_info->rp_action_code ] = $rp_info->rp_privilege;
         }
-        
-        
-        
+
+
+
         return $db_result_array;
     }
-    
-    
-    
+
+
+
     public static function ResetRolePrivileges($role_id)
-    { 
+    {
         DB::table('role_privileges')->where('fk_role_r_id', '=', $role_id)->delete();
     }
 }
