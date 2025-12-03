4b7eb7e2974097342dee6529fec9b6493b98f0f4
Mohamad Mantach <itmsolutionslb@gmail.com>
2025-12-03 20:44:25 +0200

    Fix fnb item forms and fix payment vouchers

--- resources/views/dashboard/empty.blade.php
+++ resources/views/dashboard/empty.blade.php
@@ -1,12 +1,49 @@
 <?php
 /***********************************************************
- * empty.blade.php
- * Product :titanerp
- * Version : 1.0
- * Release : 1
- * Date Created : 10/12/2025
- * Developed By  : Mohamad Mantach   PHP Department itm Solutions
- * All Rights Reserved ,   itm Solutions COPYRIGHT 2025
- *
- * Page Description :
+maindashboard.blade.php
+Product : titan HMIS
+Version : 1.0
+Release : 2
+Date Created Nov 3, 2023
+Developed By  : Mohamad Mantach   PHP Department itm Solutions
+All Rights Reserved ,   itm Solutions COPYRIGHT 2023
+
+Page Description :
+{Enter page description Here}
  ***********************************************************/
+
+?>
+@extends('layouts.layout',['page_title' => "Dashboard"])
+
+@section('plugins')
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/continentsLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/usaLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZoneAreasLow.js"></script>
+
+    <script src="{{ url('default/assets/app/js/dashboard.js') }}" type="text/javascript"></script>
+@endsection
+@section('themes')
+    <link href="{{ url('admin/assets/vendors/base/vendors.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ url('admin/assets/assets/demo/default/base/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <style>
+        .amcharts-export-menu.amcharts-export-menu-top-right.amExportButton {
+            display: none;
+        }
+
+        #chartsalesbyproducts {
+            width: 100%;
+            height: 500px;
+        }
+    </style>
+@endsection
+
+@section('content')
+@endsection
